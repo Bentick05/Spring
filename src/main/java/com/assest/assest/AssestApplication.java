1d91ef80bc8ad644ca5c8604b6ecb306de18a969
gokul.sathyamurty <v-gsathyamurty@expediagroup.com>
2023-10-11 16:22:03 +0530

    Assest_Management_Login_Page

---
+++ src/main/java/com/assest/assest/AssestApplication.java
@@ -0,0 +1,14 @@
+package com.assest.assest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class AssestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assest/assest/AssestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assest.assest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class AssestApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
